8865bfe4410886ef770bfa797aee1b7e5626b754
Ravil Gataullin <ravil.gataullin@kaspersky.com>
2017-08-31 13:19:03 +0300

    Refactor utils module.

--- tensorflow_oop/utils.py
+++ tensorflow_oop/utils.py
@@ -5,11 +5,24 @@ Helpful functions.
 import tensorflow as tf
 
 def devices_list():
-    """List of available devices."""
+    """List of available devices.
+
+    Return:
+        result -- tuple of pairs (name, physical_device_desc)
+
+    """
     local_device_protos = tf.python.client.device_lib.list_local_devices()
-    return [x.name for x in local_device_protos]
+    return [(x.name, x.physical_device_desc) for x in local_device_protos]
 
 def checkpoints_list(log_dir):
-    """List of all model checkpoint paths."""
+    """List of all model checkpoint paths.
+
+    Arguments:
+        log_dir -- path to logging directory
+
+    Return:
+        result -- tuple of checkpoint paths
+
+    """
     checkpoint_state = tf.train.get_checkpoint_state(log_dir)
     return checkpoint_state.all_model_checkpoint_paths
