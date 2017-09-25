816952d271d19d2fe2202f5650f8d4cd4860d250
Ravil Gataullin <ravil.gataullin@kaspersky.com>
2017-09-25 21:24:33 +0300

    Start working with freeze.

--- tensorflow_oop/utils.py
+++ tensorflow_oop/utils.py
@@ -27,3 +27,55 @@ def checkpoints_list(log_dir):
     """
     checkpoint_state = tf.train.get_checkpoint_state(log_dir)
     return checkpoint_state.all_model_checkpoint_paths
+
+
+def freeze_session(filename, sess,
+                   output_node_names,
+                   variable_names_whitelist=None,
+                   variable_names_blacklist=None):
+    """Save session graph as binary file.
+
+    Arguments:
+        filename -- output path
+        sess -- session with variables and graph
+        output_node_names -- list of name strings for the result nodes of the graph
+        variable_names_whitelist -- the set of variable names to convert
+                                    (by default, all variables are converted)
+        variable_names_blacklist -- the set of variable names to omit converting to constants
+
+    """
+    input_graph_def = sess.graph.as_graph_def()
+    output_graph_def = tensorflow.graph_util.convert_variables_to_constants(
+        sess,
+        input_graph_def,
+        output_node_names,
+        variable_names_whitelist=None,
+        variable_names_blacklist=None)
+    with tf.gfile.GFile(filename, "wb") as f:
+        f.write(output_graph_def.SerializeToString())
+
+
+def freeze_checkpoint(filename, checkpoint_path,
+                      output_node_names,
+                      variable_names_whitelist=None,
+                      variable_names_blacklist=None):
+    """Save session graph as binary file.
+
+    Arguments:
+        filename -- output path
+        sess -- session with variables and graph
+        output_node_names -- list of name strings for the result nodes of the graph
+        variable_names_whitelist -- the set of variable names to convert
+                                    (by default, all variables are converted)
+        variable_names_blacklist -- the set of variable names to omit converting to constants
+
+    """
+    input_graph_def = sess.graph.as_graph_def()
+    output_graph_def = tensorflow.graph_util.convert_variables_to_constants(
+        sess,
+        input_graph_def,
+        output_node_names,
+        variable_names_whitelist=None,
+        variable_names_blacklist=None)
+    with tf.gfile.GFile(filename, "wb") as f:
+        f.write(output_graph_def.SerializeToString())
