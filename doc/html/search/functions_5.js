e18fba7810af6a6d0a02d7781563f7b4744476f7
Ravil Gataullin <ravil.gataullin@kaspersky.com>
2017-10-02 20:31:33 +0300

    Update documentation.

--- doc/html/search/functions_5.js
+++ doc/html/search/functions_5.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['fill_5ffeed_5fdict',['fill_feed_dict',['../classtensorflow__oop_1_1neural__network_1_1_t_f_neural_network.html#af167ab26c8d60f454c7553f1fae92fc6',1,'tensorflow_oop::neural_network::TFNeuralNetwork']]],
-  ['fit',['fit',['../classtensorflow__oop_1_1neural__network_1_1_t_f_neural_network.html#a274f03538acd0657922e4df2bcfda1a1',1,'tensorflow_oop::neural_network::TFNeuralNetwork']]],
+  ['fit',['fit',['../classtensorflow__oop_1_1neural__network_1_1_t_f_neural_network.html#a9a16a5319997b6529047728dcef913cc',1,'tensorflow_oop::neural_network::TFNeuralNetwork']]],
   ['forward',['forward',['../classtensorflow__oop_1_1neural__network_1_1_t_f_neural_network.html#a86816429c12b2f64731b986c5b6a2015',1,'tensorflow_oop::neural_network::TFNeuralNetwork']]],
   ['freeze_5fcheckpoint',['freeze_checkpoint',['../classtensorflow__oop_1_1utils_1_1_t_f_utils.html#a82b4ef295ca5a4cfa96a3256ccedab2d',1,'tensorflow_oop::utils::TFUtils']]],
   ['freeze_5fsession',['freeze_session',['../classtensorflow__oop_1_1utils_1_1_t_f_utils.html#a2d0676f45074233dce64a2830cef713e',1,'tensorflow_oop::utils::TFUtils']]],
